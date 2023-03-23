5bb249dd8ad4a824e1d2907d8398ee5a65da5d7f
Brent Smyth <brentsmyth8@gmail.com>
2023-03-23 16:04:45 -0400

    Adding the ability to have multiple named lists

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from 'react';
-import { Platform, KeyboardAvoidingView, StyleSheet, View, ScrollView } from 'react-native';
-import { Appbar, TextInput, Button, Checkbox } from 'react-native-paper';
+import {
+  Platform,
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  ScrollView,
+} from 'react-native';
+import {
+  Appbar,
+  TextInput,
+  Button,
+  Checkbox,
+  Menu,
+  Divider,
+  Provider as PaperProvider,
+  Dialog,
+  Portal,
+} from 'react-native-paper';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import uuid from 'react-native-uuid';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
@@ -11,18 +27,42 @@ const sortByCompleted = (list) => {
   return [...incompleteItems, ...completeItems];
 };
 
+const DEFAULT_LIST_NAME = 'todos';
+
 export default function App() {
+  const [listNames, setListNames] = useState([DEFAULT_LIST_NAME]);
+  const [listName, setListName] = useState(DEFAULT_LIST_NAME);
   const [list, setList] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [dialogVisible, setDialogVisible] = useState(false);
+  const [newListName, setNewListName] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await AsyncStorage.getItem('myList');
+        const data = await AsyncStorage.getItem(listName);
         if (data !== null) {
           setList(sortByCompleted(JSON.parse(data)));
         } else {
           setList([]);
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    fetchData();
+  }, [listName]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await AsyncStorage.getItem('listNames');
+        if (data !== null) {
+          setListNames(JSON.parse(data));
+        } else {
+          setListNames([DEFAULT_LIST_NAME]);
         }
       } catch (e) {
         console.error(e);
@@ -45,7 +85,7 @@ export default function App() {
     setList(newList);
 
     try {
-      await AsyncStorage.setItem('myList', JSON.stringify(newList));
+      await AsyncStorage.setItem(listName, JSON.stringify(newList));
     } catch (e) {
       console.error(e);
     }
@@ -63,45 +103,123 @@ export default function App() {
     setList(sortByCompleted(newList));
 
     try {
-      await AsyncStorage.setItem('myList', JSON.stringify(newList));
+      await AsyncStorage.setItem(listName, JSON.stringify(newList));
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const handleListChange = async (name) => {
+    setListName(name);
+
+    try {
+      const data = await AsyncStorage.getItem(name);
+      if (data !== null) {
+        setList(sortByCompleted(JSON.parse(data)));
+      } else {
+        setList([]);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+
+    setMenuVisible(false);
+  };
+
+  const handleAddList = () => {
+    setMenuVisible(false);
+    setDialogVisible(true);
+  };
+
+  const handleCreateList = async () => {
+    if (newListName.trim() === '') return;
+
+    const newName = newListName.trim().toLowerCase().replace(/\s+/g, '-');
+    const newNames = [...listNames, newName];
+    setListNames(newNames);
+    setDialogVisible(false);
+
+    try {
+      await AsyncStorage.setItem('listNames', JSON.stringify(newNames));
+      await AsyncStorage.setItem(newName, '[]');
+      setListName(newName);
+      setList([]);
     } catch (e) {
       console.error(e);
     }
   };
 
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <Appbar.Header>
-          <Appbar.Content title="Todos" />
-        </Appbar.Header>
-        <ScrollView>
-          {list.map((item) => (
-            <Checkbox.Item
-              key={item.uuid}
-              label={item.description}
-              status={item.complete ? 'checked' : 'unchecked'}
-              onPress={() => handleCompleteItem(item.uuid)}
+    <PaperProvider>
+      <SafeAreaProvider>
+        <View style={styles.container}>
+          <Appbar.Header>
+            <Appbar.Content title={listName} />
+            <Menu
+              visible={menuVisible}
+              onDismiss={() => setMenuVisible(false)}
+              anchor={
+                <Appbar.Action
+                  icon="dots-vertical"
+                  onPress={() => setMenuVisible(true)}
+                />
+              }
+            >
+              {listNames.map((name) => (
+                <Menu.Item
+                  key={name}
+                  title={name}
+                  onPress={() => handleListChange(name)}
+                />
+              ))}
+              <Divider />
+              <Menu.Item title="Add list" onPress={handleAddList} />
+            </Menu>
+          </Appbar.Header>
+          <ScrollView>
+            {list.map((item) => (
+              <Checkbox.Item
+                key={item.uuid}
+                label={item.description}
+                status={item.complete ? 'checked' : 'unchecked'}
+                onPress={() => handleCompleteItem(item.uuid)}
+              />
+            ))}
+          </ScrollView>
+          <KeyboardAvoidingView
+            style={styles.footer}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
+          >
+            <TextInput
+              label="Add item"
+              value={inputValue}
+              onChangeText={(text) => setInputValue(text)}
+              style={styles.input}
             />
-          ))}
-        </ScrollView>
-        <KeyboardAvoidingView
-         style={styles.footer}
-         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-         keyboardVerticalOffset={Platform.OS === 'ios' ? 64 : 0}
-        >
-          <TextInput
-            label="Add item"
-            value={inputValue}
-            onChangeText={(text) => setInputValue(text)}
-            style={styles.input}
-          />
-          <Button mode="contained" onPress={handleAddItem}>
-            Add
-          </Button>
-        </KeyboardAvoidingView>
-      </View>
-    </SafeAreaProvider>
+            <Button mode="contained" onPress={handleAddItem}>
+              Add
+            </Button>
+          </KeyboardAvoidingView>
+          <Portal>
+            <Dialog visible={dialogVisible} onDismiss={() => setDialogVisible(false)}>
+              <Dialog.Title>Create new list</Dialog.Title>
+              <Dialog.Content>
+                <TextInput
+                  label="List name"
+                  value={newListName}
+                  onChangeText={(text) => setNewListName(text)}
+                />
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button onPress={() => setDialogVisible(false)}>Cancel</Button>
+                <Button onPress={handleCreateList}>Create</Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </View>
+      </SafeAreaProvider>
+    </PaperProvider>
   );
 }
 
