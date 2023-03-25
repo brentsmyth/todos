7f800bdacc680ce94355db498702bb4599cc3b89
Brent Smyth <brentsmyth8@gmail.com>
2023-03-25 16:11:18 -0400

    Refactoring async storage logic into a hook

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   Platform,
   KeyboardAvoidingView,
@@ -17,111 +17,38 @@ import {
   Dialog,
   Portal,
 } from 'react-native-paper';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import uuid from 'react-native-uuid';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-const sortByCompleted = (list) => {
-  const incompleteItems = list.filter((item) => !item.complete);
-  const completeItems = list.filter((item) => item.complete);
-  return [...incompleteItems, ...completeItems];
-};
-
-const DEFAULT_LIST_NAME = 'Todos';
+import { useAsyncStorage } from './hooks/useAsyncStorage';
 
 export default function App() {
-  const [lists, setLists] = useState([]);
-  const [listUuid, setListUuid] = useState('');
-  const [list, setList] = useState([]);
   const [inputValue, setInputValue] = useState('');
   const [menuVisible, setMenuVisible] = useState(false);
   const [dialogVisible, setDialogVisible] = useState(false);
   const [newListName, setNewListName] = useState('');
 
+  const {
+    lists,
+    listUuid,
+    list,
+    addItem,
+    completeItem,
+    changeList,
+    addList,
+  } = useAsyncStorage();
+
   const currentList = lists.find(list => list.uuid === listUuid);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let lists;
-        const listsData = await AsyncStorage.getItem('lists');
-        if (listsData !== null) {
-          lists = JSON.parse(listsData);
-        } else {
-          lists = [{
-            name: DEFAULT_LIST_NAME,
-            uuid: uuid.v4(),
-          }];
-        }
-
-        setLists(lists);
-        setListUuid(lists[0].uuid);
-
-        const listData = await AsyncStorage.getItem(lists[0].uuid);
-        if (listData !== null) {
-          setList(sortByCompleted(JSON.parse(listData)));
-        } else {
-          setList([]);
-        }
-      } catch (e) {
-        console.error(e);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleAddItem = async () => {
-    if (inputValue.trim() === '') return;
-
-    const newItem = {
-      uuid: uuid.v4(),
-      description: inputValue,
-      complete: false,
-    };
-
-    const newList = sortByCompleted([...list, newItem]);
-    setList(newList);
-
-    try {
-      await AsyncStorage.setItem(listUuid, JSON.stringify(newList));
-    } catch (e) {
-      console.error(e);
-    }
-
+  const handleAddItem = () => {
+    addItem(inputValue);
     setInputValue('');
   };
 
-  const handleCompleteItem = async (uuid) => {
-    const index = list.findIndex((item) => item.uuid === uuid);
-
-    if (index === -1) return;
-
-    const newList = [...list];
-    newList[index].complete = true;
-    setList(sortByCompleted(newList));
-
-    try {
-      await AsyncStorage.setItem(listUuid, JSON.stringify(newList));
-    } catch (e) {
-      console.error(e);
-    }
+  const handleCompleteItem = (uuid) => {
+    completeItem(uuid);
   };
 
-  const handleListChange = async (uuid) => {
-    setListUuid(uuid);
-
-    try {
-      const data = await AsyncStorage.getItem(uuid);
-      if (data !== null) {
-        setList(sortByCompleted(JSON.parse(data)));
-      } else {
-        setList([]);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-
+  const handleListChange = (uuid) => {
+    changeList(uuid);
     setMenuVisible(false);
   };
 
@@ -130,25 +57,10 @@ export default function App() {
     setDialogVisible(true);
   };
 
-  const handleCreateList = async () => {
-    if (newListName.trim() === '') return;
-
-    const newList = {
-      name: newListName.trim(),
-      uuid: uuid.v4(),
-    };
-    const newLists = [...lists, newList];
-    setLists(newLists);
+  const handleCreateList = () => {
+    addList(newListName);
     setDialogVisible(false);
-
-    try {
-      await AsyncStorage.setItem('lists', JSON.stringify(newLists));
-      await AsyncStorage.setItem(newList.uuid, '[]');
-      setListUuid(newList.uuid);
-      setList([]);
-    } catch (e) {
-      console.error(e);
-    }
+    setNewListName('');
   };
 
   return (
