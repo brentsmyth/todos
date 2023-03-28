fd03ded9a75fae6959bb5c857631a95dfa345b0e
Brent Smyth <brentsmyth8@gmail.com>
2023-03-28 11:44:15 -0400

    Updating app structure to standard react `src` path

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
 import { StyleSheet, View } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { TodoProvider } from './contexts/TodoContext';
-import Header from './components/Header';
-import TodoList from './components/TodoList';
-import AddItem from './components/AddItem';
+import { TodoProvider } from './src/contexts/TodoContext';
+import Header from './src/components/Header';
+import TodoList from './src/components/TodoList';
+import AddItem from './src/components/AddItem';
 
 export default function App() {
   return (
