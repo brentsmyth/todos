e9157013335e3e701bdc6610a34b4bcd3522fc25
Brent Smyth <brentsmyth8@gmail.com>
2023-03-29 10:52:45 -0400

    Replacing Dialog component with simplified approach

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
 import { useState, useCallback } from 'react';
-import { Keyboard } from 'react-native';
-import { Appbar, Menu, Divider } from 'react-native-paper';
-import CreateListDialog from './CreateListDialog';
+import { Keyboard, StyleSheet, View } from 'react-native';
+import { Appbar, Menu, Divider, TextInput, Portal, Button } from 'react-native-paper';
 import { useTodoContext } from '../contexts/TodoContext';
 
 const Header = () => {
   const { listUuid, lists, changeList, addList } = useTodoContext();
   const [menuVisible, setMenuVisible] = useState(false);
-  const [dialogVisible, setDialogVisible] = useState(false);
+  const [addListViewVisible, setAddListViewVisible] = useState(false);
+  const [newListName, setNewListName] = useState('');
   const currentList = lists.find((list) => list.uuid === listUuid);
 
   const handleMenuOpen = useCallback(() => {
@@ -25,45 +25,77 @@ const Header = () => {
 
   const handleAddList = useCallback(() => {
     setMenuVisible(false);
-    setDialogVisible(true);
+    setAddListViewVisible(true);
   }, []);
 
-  const handleCreateList = useCallback((newListName) => {
+  const handleCreateList = useCallback(() => {
     addList(newListName);
-    setDialogVisible(false);
-  }, [addList]);
+    setNewListName('');
+    setAddListViewVisible(false);
+  }, [addList, newListName]);
 
   return (
-    <Appbar.Header>
-      <Appbar.Content title={currentList?.name} />
-      <Menu
-        visible={menuVisible}
-        onDismiss={() => setMenuVisible(false)}
-        anchor={
-          <Appbar.Action
-            icon="dots-vertical"
-            onPress={handleMenuOpen}
+    <>
+      <Appbar.Header>
+        <Appbar.Content title={currentList?.name} />
+        <Menu
+          visible={menuVisible}
+          onDismiss={() => setMenuVisible(false)}
+          anchor={
+            <Appbar.Action
+              icon="dots-vertical"
+              onPress={handleMenuOpen}
+            />
+          }
+        >
+          {lists.map((list) => (
+            <Menu.Item
+              key={list.uuid}
+              title={list.name}
+              onPress={() => handleListChange(list.uuid)}
+            />
+          ))}
+          <Divider />
+          <Menu.Item title="Add list" onPress={handleAddList} />
+        </Menu>
+      </Appbar.Header>
+
+      {addListViewVisible && (
+        <View style={styles.addListWrapper}>
+          <TextInput
+            label="Enter a new list name"
+            mode="outlined"
+            style={styles.addListInput}
+            autoFocus={true}
+            onChangeText={(newListName) => setNewListName(newListName)}
+            value={newListName}
+            returnKeyType="done"
+            onSubmitEditing={handleCreateList}
           />
-        }
-      >
-        {lists.map((list) => (
-          <Menu.Item
-            key={list.uuid}
-            title={list.name}
-            onPress={() => handleListChange(list.uuid)}
-          />
-        ))}
-        <Divider />
-        <Menu.Item title="Add list" onPress={handleAddList} />
-      </Menu>
-      <CreateListDialog
-        visible={dialogVisible}
-        onCreate={handleCreateList}
-        onCancel={() => setDialogVisible(false)}
-      />
-    </Appbar.Header>
+          <Button onPress={() => setAddListViewVisible(false)}>
+            Cancel
+          </Button>
+        </View>
+      )}
+    </>
   );
 };
+
+const styles = StyleSheet.create({
+  addListWrapper: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    zIndex: 1,
+    paddingTop: 120,
+    backgroundColor: 'rgba(255, 255, 255, 0.9)',
+  },
+  addListInput: {
+    margin: 8,
+  },
+});
 
 export default Header;
 
